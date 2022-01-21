1f0c7b70a033751c56915643f74a8fc0183ac82c
HYUN816 <leehg95@gmail.com>
2022-01-21 12:11:53 +0900

    initial commit

---
+++ MLserver/discover/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+
+import json
+
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
