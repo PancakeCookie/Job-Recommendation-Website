0d4d9c04897a27850490055cbadefc6856d4e111
HYUN816 <89058170+HYUN816@users.noreply.github.com>
2022-01-21 12:05:30 +0900

    Delete MLserver directory

--- MLserver/discover/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from discover.models import knowcode
-
-admin.site.register(knowcode)
--- MLserver/jobrec/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JobrecConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jobrec'
